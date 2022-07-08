530ca8ef930555eaaf4273d35bfe21eafd602ad6
programmingfreshers <77062180+programmingfreshers@users.noreply.github.com>
2022-07-08 20:18:08 +0530

    changes

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 let { SmartAPI, WebSocket } = require("smartapi-javascript");
 const app = express();
-const port = 5000;
+const port = 3000;
 
 const stockDetails = [
   {
